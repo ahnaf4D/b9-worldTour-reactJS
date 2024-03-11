7cf0e36309b49cbc5f07678cfa322ed5816ced1d
ahnaf4D <ahnaftahmid802@gmail.com>
2024-03-11 13:36:56 +0600

    country data display on the ui

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,13 @@
 import './Country.css'
 const Country = ({ country }) => {
   console.log(country);
-  const { name, flags } = country;
+  const { name, flags, population, area } = country;
   return (
     <div className='country'>
       <img src={flags.png} alt={flags.alt} />
       <h3>Name : {name.common}</h3>
+      <p>Population : {population}</p>
+      <p>Area : {area}</p>
     </div>
   );
 };
